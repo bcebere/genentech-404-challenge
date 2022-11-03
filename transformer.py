6886b7126a521fe3ea1039e73c9d0f3be2cbb605
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-11-03 13:15:15 +0000

    add helper

--- transformer.py
+++ transformer.py
@@ -1,311 +1,121 @@
-# stdlib
-import math
-import copy
-
 # third party
 import torch
-import torch.nn.functional as F
+import math
 from torch import nn
-from torch.autograd import Variable
-
+from torch.nn.modules.transformer import TransformerEncoder, TransformerEncoderLayer
+from typing import Any, Optional
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def nopeak_mask(size, opt):
-    np_mask = np.triu(np.ones((1, size, size)), k=1).astype("uint8")
-    np_mask = Variable(torch.from_numpy(np_mask) == 0)
-    if opt.device == 0:
-        np_mask = np_mask.cuda()
-    return np_mask
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.scale = nn.Parameter(torch.ones(1))
 
-
-def create_masks(src, trg, opt):
-
-    src_mask = (src != opt.src_pad).unsqueeze(-2)
-
-    if trg is not None:
-        trg_mask = (trg != opt.trg_pad).unsqueeze(-2)
-        size = trg.size(1)  # get seq_len for matrix
-        np_mask = nopeak_mask(size, opt)
-        if trg.is_cuda:
-            np_mask.cuda()
-        trg_mask = trg_mask & np_mask
-
-    else:
-        trg_mask = None
-    return src_mask, trg_mask
-
-
-class Embedder(nn.Module):
-    def __init__(self, n_units_input: int, n_units_hidden: int = 500):
-        super().__init__()
-        self.n_units_hidden = n_units_hidden
-        self.embed = nn.Embedding(n_units_input, n_units_hidden)
-
-    def forward(self, x):
-        return self.embed(x)
-
-
-class PositionalEncoder(nn.Module):
-    def __init__(
-        self, n_units_hidden: int = 500, max_seq_len: int = 500, dropout: float = 0
-    ):
-        super().__init__()
-        self.n_units_hidden = n_units_hidden
-        self.dropout = nn.Dropout(dropout)
-        # create constant 'pe' matrix with values dependant on
-        # pos and i
-        pe = torch.zeros(max_seq_len, n_units_hidden)
-        for pos in range(max_seq_len):
-            for i in range(0, n_units_hidden, 2):
-                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / n_units_hidden)))
-                pe[pos, i + 1] = math.cos(
-                    pos / (10000 ** ((2 * (i + 1)) / n_units_hidden))
-                )
-        pe = pe.unsqueeze(0)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
         self.register_buffer("pe", pe)
 
-    def forward(self, x):
-        # make embeddings relatively larger
-        x = x * math.sqrt(self.n_units_hidden)
-        # add constant to embedding
-        seq_len = x.size(1)
-        pe = Variable(self.pe[:, :seq_len], requires_grad=False).to(DEVICE)
-        x = x + pe
+    def forward(self, x, horizons):
+        x = x + self.scale * self.pe[: x.size(0), :]
         return self.dropout(x)
-
-
-class Norm(nn.Module):
-    def __init__(self, n_units_hidden: int = 500, eps: float = 1e-6):
-        super().__init__()
-
-        self.size = n_units_hidden
-
-        # create two learnable parameters to calibrate normalisation
-        self.alpha = nn.Parameter(torch.ones(self.size))
-        self.bias = nn.Parameter(torch.zeros(self.size))
-
-        self.eps = eps
-
-    def forward(self, x):
-        norm = (
-            self.alpha
-            * (x - x.mean(dim=-1, keepdim=True))
-            / (x.std(dim=-1, keepdim=True) + self.eps)
-            + self.bias
-        )
-        return norm
-
-
-def attention(q, k, v, d_k, mask=None, dropout=None):
-
-    scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
-
-    if mask is not None:
-        mask = mask.unsqueeze(1)
-        scores = scores.masked_fill(mask == 0, -1e9)
-
-    scores = F.softmax(scores, dim=-1)
-
-    if dropout is not None:
-        scores = dropout(scores)
-
-    output = torch.matmul(scores, v)
-    return output
-
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, n_heads: int = 1, n_units_hidden: int = 500, dropout: float = 0):
-        super().__init__()
-
-        self.n_units_hidden = n_units_hidden
-        self.d_k = n_units_hidden // n_heads
-        self.h = n_heads
-
-        self.q_linear = nn.Linear(n_units_hidden, n_units_hidden)
-        self.v_linear = nn.Linear(n_units_hidden, n_units_hidden)
-        self.k_linear = nn.Linear(n_units_hidden, n_units_hidden)
-
-        self.dropout = nn.Dropout(dropout)
-        self.out = nn.Linear(n_units_hidden, n_units_hidden)
-
-    def forward(self, q, k, v, mask=None):
-
-        bs = q.size(0)
-
-        # perform linear operation and split into N n_heads
-        k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
-        q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
-        v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
-
-        # transpose to get dimensions bs * N * sl * n_units_hidden
-        k = k.transpose(1, 2)
-        q = q.transpose(1, 2)
-        v = v.transpose(1, 2)
-
-        # calculate attention using function we will define next
-        scores = attention(q, k, v, self.d_k, mask, self.dropout)
-        # concatenate n_heads and put through final linear layer
-        concat = scores.transpose(1, 2).contiguous().view(bs, -1, self.n_units_hidden)
-        output = self.out(concat)
-
-        return output
-
-
-class FeedForward(nn.Module):
-    def __init__(
-        self, n_units_input: int, n_units_hidden: int = 500, dropout: float = 0
-    ):
-        super().__init__()
-
-        # We set d_units_hidden as a default to 500
-        self.linear_1 = nn.Linear(n_units_input, n_units_hidden)
-        self.dropout = nn.Dropout(dropout)
-        self.linear_2 = nn.Linear(n_units_hidden, n_units_input)
-
-    def forward(self, x: torch.Tensor):
-        x = self.dropout(F.relu(self.linear_1(x)))
-        x = self.linear_2(x)
-        return x
-
-
-class EncoderLayer(nn.Module):
-    def __init__(self, n_units_hidden: int = 500, n_heads: int = 1, dropout: float = 0):
-        super().__init__()
-        self.norm_1 = Norm(n_units_hidden)
-        self.norm_2 = Norm(n_units_hidden)
-        self.attn = MultiHeadAttention(n_heads, n_units_hidden, dropout=dropout)
-        self.ff = FeedForward(n_units_hidden, dropout=dropout)
-        self.dropout_1 = nn.Dropout(dropout)
-        self.dropout_2 = nn.Dropout(dropout)
-
-    def forward(self, x, mask):
-        x2 = self.norm_1(x)
-        x = x + self.dropout_1(self.attn(x2, x2, x2, mask))
-        x2 = self.norm_2(x)
-        x = x + self.dropout_2(self.ff(x2))
-        return x
-
-
-# build a decoder layer with two multi-head attention layers and
-# one feed-forward layer
-class DecoderLayer(nn.Module):
-    def __init__(self, n_units_hidden: int = 500, n_heads: int = 1, dropout: float = 0):
-        super().__init__()
-        self.norm_1 = Norm(n_units_hidden)
-        self.norm_2 = Norm(n_units_hidden)
-        self.norm_3 = Norm(n_units_hidden)
-
-        self.dropout_1 = nn.Dropout(dropout)
-        self.dropout_2 = nn.Dropout(dropout)
-        self.dropout_3 = nn.Dropout(dropout)
-
-        self.attn_1 = MultiHeadAttention(n_heads, n_units_hidden, dropout=dropout)
-        self.attn_2 = MultiHeadAttention(n_heads, n_units_hidden, dropout=dropout)
-        self.ff = FeedForward(n_units_hidden, dropout=dropout)
-
-    def forward(self, x, e_outputs, src_mask, trg_mask):
-        x2 = self.norm_1(x)
-        x = x + self.dropout_1(self.attn_1(x2, x2, x2, trg_mask))
-        x2 = self.norm_2(x)
-        x = x + self.dropout_2(self.attn_2(x2, e_outputs, e_outputs, src_mask))
-        x2 = self.norm_3(x)
-        x = x + self.dropout_3(self.ff(x2))
-        return x
-
-
-def get_clones(module, N):
-    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
-
-
-class Encoder(nn.Module):
-    def __init__(
-        self,
-        n_units_input: int,
-        n_units_hidden: int = 500,
-        n_layers_hidden: int = 2,
-        n_heads: int = 1,
-        dropout: float = 0,
-    ):
-        super().__init__()
-        self.n_layers_hidden = n_layers_hidden
-        self.embed = Embedder(n_units_input, n_units_hidden)
-        self.pe = PositionalEncoder(n_units_hidden, dropout=dropout)
-        self.layers = get_clones(
-            EncoderLayer(n_units_hidden, n_heads, dropout), n_layers_hidden
-        )
-        self.norm = Norm(n_units_hidden)
-
-    def forward(self, src, mask):
-        x = self.embed(src)
-        x = self.pe(x)
-        for i in range(self.n_layers_hidden):
-            x = self.layers[i](x, mask)
-        return self.norm(x)
-
-
-class Decoder(nn.Module):
-    def __init__(
-        self,
-        n_units_input: int,
-        n_units_hidden: int = 100,
-        n_layers_hidden: int = 2,
-        n_heads: int = 1,
-        dropout: float = 0,
-    ):
-        super().__init__()
-        self.n_layers_hidden = n_layers_hidden
-        self.embed = Embedder(n_units_input, n_units_hidden)
-        self.pe = PositionalEncoder(n_units_hidden, dropout=dropout)
-        self.layers = get_clones(
-            DecoderLayer(n_units_hidden, n_heads, dropout), n_layers_hidden
-        )
-        self.norm = Norm(n_units_hidden)
-
-    def forward(self, trg, e_outputs, src_mask, trg_mask):
-        x = self.embed(trg)
-        x = self.pe(x)
-        for i in range(self.n_layers_hidden):
-            x = self.layers[i](x, e_outputs, src_mask, trg_mask)
-        return self.norm(x)
 
 
 class Transformer(nn.Module):
     def __init__(
         self,
-        n_units_input: int,
-        n_units_output: int,
-        n_units_hidden: int = 500,
+        n_units_in: int,
+        n_units_out: int,
+        n_units_hidden: int = 512,
+        n_head: int = 1,
+        d_ffn: int = 128,
+        dropout: float = 0.1,
+        activation="relu",
         n_layers_hidden: int = 2,
-        n_heads: int = 1,
-        dropout: float = 0,
     ):
-        super().__init__()
-        self.encoder = Encoder(
-            n_units_input,
-            n_units_hidden=n_units_hidden,
-            n_layers_hidden=n_layers_hidden,
-            n_heads=n_heads,
-            dropout=dropout,
-        )
-        self.decoder = Decoder(
-            n_units_output,
-            n_units_hidden=n_units_hidden,
-            n_layers_hidden=n_layers_hidden,
-            n_heads=n_heads,
-            dropout=dropout,
-        )
-        self.out = nn.Linear(n_units_hidden, n_units_output)
+        """
+        Args:
+            n_units_in: the number of features (aka variables, dimensions, channels) in the time series dataset
+            n_units_out: the number of target classes
+            n_units_hidden: total dimension of the model.
+            nhead:  parallel attention heads.
+            d_ffn: the dimension of the feedforward network model.
+            dropout: a Dropout layer on attn_output_weights.
+            activation: the activation function of intermediate layer, relu or gelu.
+            num_layers: the number of sub-encoder-layers in the encoder.
 
-    def forward(
-        self,
-        src_data: torch.Tensor,
-        src_mask: torch.Tensor,
-        target_data: torch.Tensor,
-        target_mask: torch.Tensor,
-    ):
-        e_outputs = self.encoder(src_data, src_mask)
-        d_output = self.decoder(trg, e_outputs, src_mask, src_mask)
-        output = self.out(d_output)
-        return output
+        Input shape:
+            bs (batch size) x nvars (aka variables, dimensions, channels) x seq_len (aka time steps)
+        """
+        self.permute = Permute(2, 0, 1)
+        self.inlinear = nn.Linear(n_units_in, n_units_hidden)
+        self.relu = nn.ReLU()
+        encoder_layer = TransformerEncoderLayer(
+            n_units_hidden,
+            n_head,
+            dim_feedforward=d_ffn,
+            dropout=dropout,
+            activation=activation,
+        )
+        encoder_norm = nn.LayerNorm(n_units_hidden)
+        self.transformer_encoder = TransformerEncoder(
+            encoder_layer, n_layers_hidden, norm=encoder_norm
+        )
+        self.transpose = Transpose(1, 0)
+        self.max = Max(1)
+        self.outlinear = nn.Linear(n_units_hidden, n_units_out)
+        self.pos_encoder = PositionalEncoding()
+
+    def forward(self, x, horizons):
+        x = self.permute(x)  # bs x nvars x seq_len -> seq_len x bs x nvars
+        x = self.inlinear(x)  # seq_len x bs x nvars -> seq_len x bs x n_units_hidden
+        x = self.relu(x)
+
+        x = self.pos_encoder(x, horizons)
+
+        x = self.transformer_encoder(x)
+        x = self.transpose(
+            x
+        )  # seq_len x bs x n_units_hidden -> bs x seq_len x n_units_hidden
+        x = self.max(x)
+        x = self.relu(x)
+        x = self.outlinear(x)
+        return x
+
+
+class Permute(nn.Module):
+    def __init__(self, *dims: Any) -> None:
+        super(Permute, self).__init__()
+        self.dims = dims
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.permute(self.dims)
+
+
+class Max(nn.Module):
+    def __init__(self, dim: Optional[int] = None, keepdim: bool = False):
+        super(Max, self).__init__()
+        self.dim, self.keepdim = dim, keepdim
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.max(self.dim, keepdim=self.keepdim)[0]
+
+
+class Transpose(nn.Module):
+    def __init__(self, *dims: Any, contiguous: bool = False) -> None:
+        super(Transpose, self).__init__()
+        self.dims, self.contiguous = dims, contiguous
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.contiguous:
+            return x.transpose(*self.dims).contiguous()
+        else:
+            return x.transpose(*self.dims)
---
+++ ts_imputer_v2.py
@@ -0,0 +1,386 @@
+import random
+from typing import Any, List, Optional, Tuple
+
+import numpy as np
+import pandas as pd
+import torch
+from pydantic import validate_arguments
+from torch import nn
+from transformer import Transformer
+
+from mlp import MLP
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def enable_reproducible_results(seed: int = 0) -> None:
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+
+
+class TimeSeriesImputerTemporal(nn.Module):
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def __init__(
+        self,
+        n_units_in: int,
+        n_units_out: int,
+        n_units_hidden: int = 100,
+        n_layers_hidden: int = 2,
+        n_iter: int = 2500,
+        n_iter_print: int = 100,
+        batch_size: int = 100,
+        lr: float = 1e-3,
+        weight_decay: float = 1e-3,
+        device: Any = DEVICE,
+        nonlin_out: Optional[List[Tuple[str, int]]] = None,
+        dropout: float = 0,
+        nonlin: Optional[str] = "leaky_relu",
+        random_state: int = 0,
+        clipping_value: int = 1,
+        patience: int = 5,
+        residual: bool = True,
+    ) -> None:
+        super(TimeSeriesImputerTemporal, self).__init__()
+
+        enable_reproducible_results(random_state)
+
+        self.n_iter = n_iter
+        self.n_iter_print = n_iter_print
+        self.batch_size = batch_size
+        self.n_units_in = n_units_in
+        self.n_units_hidden = n_units_hidden
+        self.n_layers_hidden = n_layers_hidden
+        self.device = device
+        self.lr = lr
+        self.clipping_value = clipping_value
+
+        self.patience = patience
+        self.random_state = random_state
+
+        self.latent_layer = Transformer(
+            n_units_in=n_units_in,
+            n_units_out=n_units_hidden,
+            n_units_hidden=n_units_hidden,
+            d_ffn=n_units_hidden,
+            n_layers_hidden=n_layers_hidden,
+            dropout=dropout,
+            activation=nonlin,
+            random_state=random_state,
+        )
+
+        self.layer_out = MLP(
+            task_type="regression",
+            n_units_in=n_units_hidden + 1,  # latent + VISCODE
+            n_units_out=n_units_out,
+            n_layers_hidden=n_layers_hidden,
+            n_units_hidden=n_units_hidden,
+            dropout=dropout,
+            nonlin=nonlin,
+            device=device,
+            nonlin_out=nonlin_out,
+            residual=residual,
+        )
+        self.nonlin_out = nonlin_out
+
+        self.optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=lr,
+            weight_decay=weight_decay,
+        )  # optimize all rnn parameters
+
+    def forward_latent(
+        self,
+        data: torch.Tensor,
+    ) -> torch.Tensor:
+        assert torch.isnan(data).sum() == 0
+        return self.layer_latent(
+            torch.swapaxes(data, 1, 2)
+        )  # bs x seq_len x feats -> bs x feats x seq_len
+
+    def forward(
+        self,
+        data: torch.Tensor,
+        viscode: torch.Tensor,
+    ) -> torch.Tensor:
+        assert torch.isnan(data).sum() == 0
+        viscode = viscode.unsqueeze(-1)
+
+        pred = self.forward_latent(data)
+
+        pred = pred.unsqueeze(1)
+        pred = torch.repeat_interleave(pred, viscode.shape[1], dim=1)
+
+        pred_merged = torch.cat([viscode, pred], dim=2)
+
+        return self.layer_out(pred_merged)
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def predict(
+        self,
+        miss_data: pd.DataFrame,
+    ) -> Any:
+        self.eval()
+        miss_data = miss_data.copy()
+
+        miss_data = miss_data.sort_values(["RID_HASH", "VISCODE"])
+        patient_ids = miss_data["RID_HASH"].unique()
+
+        output = pd.DataFrame([], columns=self.output_cols + ["VISCODE", "RID_HASH"])
+
+        for rid in patient_ids:
+            patient_miss = miss_data[miss_data["RID_HASH"] == rid]
+            viscode = patient_miss["VISCODE"].values
+            viscode = np.expand_dims(viscode, axis=0)
+
+            patient_miss = patient_miss.drop(columns=["RID_HASH"])
+            patient_miss = np.expand_dims(patient_miss.values, axis=0)
+
+            patient_miss_t = self._check_tensor(patient_miss).float()
+            viscode_t = self._check_tensor(viscode).float()
+
+            temporal_preds = self(patient_miss_t, viscode_t)
+            temporal_preds = temporal_preds.detach().cpu().numpy().squeeze()
+
+            if len(temporal_preds.shape) == 1:
+                temporal_preds = np.expand_dims(temporal_preds, axis=0)
+
+            temporal_preds = pd.DataFrame(
+                temporal_preds, columns=self.output_cols_temporal
+            )
+            temporal_preds["VISCODE"] = viscode.squeeze()
+            temporal_preds["RID_HASH"] = rid
+
+            output = pd.concat([output, temporal_preds], ignore_index=True)
+
+            assert output.isna().sum().sum() == 0
+
+        output.index = miss_data.index
+        output = output[["RID_HASH", "VISCODE"] + self.output_cols_temporal]
+
+        return output
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def predict_latent(
+        self,
+        miss_data: pd.DataFrame,
+    ) -> Any:
+        self.eval()
+        miss_data = miss_data.copy()
+
+        miss_data = miss_data.sort_values(["RID_HASH", "VISCODE"])
+        patient_ids = miss_data["RID_HASH"].unique()
+
+        out_cols = ["RID_HASH"] + list(range(self.n_units_hidden))
+        output = pd.DataFrame([], columns=out_cols)
+
+        for rid in patient_ids:
+            patient_miss = miss_data[miss_data["RID_HASH"] == rid]
+
+            patient_miss = patient_miss.drop(columns=["RID_HASH"])
+            patient_miss = np.expand_dims(patient_miss.values, axis=0)
+
+            patient_miss_t = self._check_tensor(patient_miss).float()
+
+            preds = self.forward_latent(patient_miss_t).detach().cpu().numpy().squeeze()
+            if len(preds.shape) == 1:
+                preds = np.expand_dims(preds, axis=0)
+
+            preds = pd.DataFrame(preds, columns=list(range(self.n_units_hidden)))
+            preds["RID_HASH"] = rid
+
+            output = pd.concat([output, preds], ignore_index=True)
+
+            assert output.isna().sum().sum() == 0
+
+        output = output[out_cols]
+
+        return output
+
+    def eval_loss(self, preds, gt, nonlin_out=None, debug=False):
+        loss = torch.tensor(0).to(self.device).float()
+
+        if preds.shape[-1] == 0:
+            return loss
+
+        if nonlin_out is None:
+            loss = nn.HuberLoss()(preds, gt)
+        else:
+            split = 0
+
+            sanity_size = 0
+            for _, step in nonlin_out:
+                sanity_size += step
+
+            assert sanity_size == preds.shape[-1]
+            assert sanity_size == gt.shape[-1]
+            for activation, step in nonlin_out:
+                if activation == "softmax":
+                    loss_fn = nn.CrossEntropyLoss()
+                else:
+                    loss_fn = nn.HuberLoss()
+                local_loss = loss_fn(
+                    preds[..., split : split + step],
+                    gt[..., split : split + step],
+                )
+
+                if debug:
+                    print(activation, step, local_loss)
+                loss += local_loss
+
+                split += step
+
+        return loss
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def fit(
+        self,
+        train_miss_data: pd.DataFrame,
+        train_real_data: pd.DataFrame,
+        val_miss_data: pd.DataFrame,
+        val_real_data: pd.DataFrame,
+    ) -> Any:
+        assert len(train_miss_data) == len(train_real_data)
+
+        train_miss_data = train_miss_data.sort_values(
+            ["RID_HASH", "VISCODE"]
+        ).reset_index(drop=True)
+        train_real_data = train_real_data.sort_values(
+            ["RID_HASH", "VISCODE"]
+        ).reset_index(drop=True)
+
+        val_miss_data = val_miss_data.sort_values(["RID_HASH", "VISCODE"]).reset_index(
+            drop=True
+        )
+        val_real_data = val_real_data.sort_values(["RID_HASH", "VISCODE"]).reset_index(
+            drop=True
+        )
+
+        patient_ids = train_miss_data["RID_HASH"].unique()
+
+        batches_by_size = {}
+        for rid in patient_ids:
+            patient_miss = train_miss_data[train_miss_data["RID_HASH"] == rid]
+
+            patient_seq_len = len(patient_miss)
+            if patient_seq_len not in batches_by_size:
+                batches_by_size[patient_seq_len] = {
+                    "input": [],
+                    "viscode": [],
+                    "gt": [],
+                }
+
+            patient_gt = train_real_data[train_real_data["RID_HASH"] == rid]
+
+            viscode = patient_gt["VISCODE"].values
+
+            viscode = np.expand_dims(viscode, axis=0)
+
+            patient_miss = patient_miss.drop(columns=["RID_HASH"])
+            patient_miss = np.expand_dims(patient_miss.values, axis=0)
+
+            patient_gt = patient_gt.drop(columns=["RID_HASH", "VISCODE"])
+
+            self.output_cols = list(patient_gt.columns)
+            patient_gt = np.expand_dims(patient_gt.values, axis=0)
+
+            patient_miss_t = self._check_tensor(patient_miss).float()
+            patient_gt_t = self._check_tensor(patient_gt).float()
+            viscode_t = self._check_tensor(viscode).float()
+
+            assert viscode_t.shape[1] == patient_gt.shape[1]
+            assert viscode_t.shape[1] > 0, patient_gt
+
+            batches_by_size[patient_seq_len]["input"].append(patient_miss_t)
+            batches_by_size[patient_seq_len]["viscode"].append(viscode_t)
+            batches_by_size[patient_seq_len]["gt"].append(patient_gt_t)
+
+        batches = []
+        for seq_len in batches_by_size:
+            seq_miss_t = torch.cat(batches_by_size[seq_len]["input"])
+            seq_viscode_t = torch.cat(batches_by_size[seq_len]["viscode"])
+            seq_gt_temporal_t = torch.cat(batches_by_size[seq_len]["gt_temporal"])
+
+            batches.append((seq_miss_t, seq_gt_temporal_t, seq_viscode_t))
+
+        patience = 0
+        best_val_loss = 999
+
+        for epoch in range(self.n_iter):
+            self.train()
+            losses = []
+
+            for (
+                patient_miss_t,
+                patient_gt_temporal_t,
+                viscode_t,
+            ) in batches:
+                self.optimizer.zero_grad()
+
+                temporal_preds = self(patient_miss_t, viscode_t)
+
+                assert temporal_preds.shape == patient_gt_temporal_t.shape
+
+                loss = self.eval_loss(
+                    temporal_preds,
+                    patient_gt_temporal_t,
+                    nonlin_out=self.nonlin_out_temporal,
+                )
+
+                assert not torch.isnan(loss)
+
+                loss.backward()  # backpropagation, compute gradients
+
+                if self.clipping_value > 0:
+                    torch.nn.utils.clip_grad_norm_(
+                        self.parameters(), self.clipping_value
+                    )
+                self.optimizer.step()  # apply gradients
+                losses.append(loss.item())
+
+            if (epoch + 1) % self.n_iter_print == 0:
+                self.eval()
+                gt_cols = list(val_real_data.columns)
+
+                with torch.no_grad():
+                    val_preds = self.predict(val_miss_data)
+                    val_preds = (
+                        val_preds[gt_cols]
+                        .drop(columns=["RID_HASH", "VISCODE"])
+                        .values.astype(float)
+                    )
+
+                    val_gt = val_real_data.drop(
+                        columns=["RID_HASH", "VISCODE"]
+                    ).values.astype(float)
+
+                    val_loss = self.eval_loss(
+                        torch.from_numpy(val_preds),
+                        torch.from_numpy(val_gt),
+                        nonlin_out=self.nonlin_out,
+                    )
+                    val_loss = val_loss.item()
+
+                if val_loss < best_val_loss:
+                    patience = 0
+                    best_val_loss = val_loss
+                else:
+                    patience += 1
+
+                if patience > self.patience:
+                    # print(f"   >>> Epoch {epoch} Early stopping...")
+                    break
+
+                print(
+                    f"   >>> Epoch {epoch} train loss  = {np.mean(losses)} val loss {val_loss}",
+                    flush=True,
+                )
+
+        self.eval()
+        return self
+
+    def _check_tensor(self, X: torch.Tensor) -> torch.Tensor:
+        if isinstance(X, torch.Tensor):
+            return X.to(self.device)
+        else:
+            return torch.from_numpy(np.asarray(X)).to(self.device)
